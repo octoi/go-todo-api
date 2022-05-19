340408c277ada8272c12664f1bcaaf24ff34eb27
fadhilsaheer <fadhilsaheer@gmail.com>
2022-05-19 17:26:17 +0530

    ..

--- model/models.go
+++ model/models.go
@@ -5,5 +5,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Todo struct {
 	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Todo       string             `json:"todo,omitempty"`
-	IsResolved bool               `json:"isResolved,omitempty"`
+	IsResolved bool               `json:"isResolved,omitempty" bson:"isResolved,omitempty"`
 }
